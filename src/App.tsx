77c25941b2349b0969bfc45e1f234fc6e69cf651
GuilhermeOM <guilhermeomalta0@gmail.com>
2022-07-09 00:54:38 -0300

    Moving transactions response values into a context.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+import { TransactionContext, TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root')
 
@@ -20,16 +21,16 @@ export function App() {
     }
 
     return (
-        <>
+        <TransactionsProvider>
             <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
             <Dashboard />
 
-            <NewTransactionModal 
+            <NewTransactionModal
                 isOpen={isNewTransactionModalOpen}
                 onRequestClose={handleCloseNewTransactionModal}
             />
 
             <GlobalStyle />
-        </>
+        </TransactionsProvider>
     );
 }
